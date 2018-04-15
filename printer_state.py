29236d48208d8d6209745ef66153a51d03e32fbe
ManuelW77 <manuel.weiser@me.com>
2018-04-15 22:26:39 +0200

    repair

--- printer_state.py
+++ printer_state.py
@@ -417,14 +417,15 @@ def on_message(client, userdata, msg):
         if printDone is True:
             if debug is True:
                 print "Watch for Power Off ----> " + str(printDone) + " - " + str(tool0_data[0]) + " - " + str(bed_data[0])
+                print "----------"
 
         if pState is False and printDone is True and tool0_data[0] < 35 and tool0_data[1] == 0 and bed_data[0] < 35 and bed_data[1] == 0:
             powerOffAll()
             client.publish("esp_tronxy_pow/relay/0/set", "0")
             printDone = False
 
-    except BaseException:
-        ex = True
+    except BaseException as err:
+        print err
 
         if debug is True:
             print "Exception: " + str(msg.payload)
